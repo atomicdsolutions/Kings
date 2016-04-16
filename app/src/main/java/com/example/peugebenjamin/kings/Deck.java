a116c904fc96e05f5b11343996410bd8aee26f03
Peuge Benjamin <support@atomicdsolutions.com>
2016-04-16 18:49:51 -0400

    Card Class

--- app/src/main/java/com/example/peugebenjamin/kings/Deck.java
+++ app/src/main/java/com/example/peugebenjamin/kings/Deck.java
@@ -1,9 +1,36 @@
 package com.example.peugebenjamin.kings;
 
+import java.util.Random;
+
 /**
  * Created by peugebenjamin on 4/5/16.
  */
 public class Deck {
+    private Cards[] cards;
+    int i;
+
+    Deck(){
+        i = 51;
+        cards = new Cards[52];
+        int x=0;
+        for (int a=0; a<=3; a++){
+            for(int b=0; b<=12; b++){
+                cards[x] = new Cards(a,b);
+            }
+        }
+    }
+
+    public Cards drawCard(){
+        Random randnum = new Random();
+        int index = 0;
+        index = randnum.nextInt(i);
+
+        Cards temp = cards[index];
+        cards[index] = cards[i];
+        cards[i]=null;
+        i--;
+        return temp;
+    }
 
 }
 
