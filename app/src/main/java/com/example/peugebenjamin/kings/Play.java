ccc6b1eaac197066b01e5ecf8d55313cb72c819f
Peuge Benjamin <Support@atomicdsolutions.com>
2016-04-19 19:07:56 -0400

    Play Class

--- app/src/main/java/com/example/peugebenjamin/kings/Play.java
+++ app/src/main/java/com/example/peugebenjamin/kings/Play.java
@@ -6,14 +6,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Play extends AppCompatActivity {
-
+    Deck gamecards;
+    Cards pulled;
+    TextView played;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        Button play = (Button) findViewById(R.id.btplay);
+        played = (TextView) findViewById(R.id.played);
+
+        gamecards = new Deck();
 
     }
 
+    public void playGame(View v){
+        if (v.equals(played)) {
+            pulled = gamecards.drawCard();
+            Toast.makeText(getApplicationContext(), pulled.toString(), Toast.LENGTH_LONG).show();
+            //played.setText(pulled.toString());
+        }
+    }
 }
