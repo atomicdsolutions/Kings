8640239848b90dfe4fec65c97771db21be22381f
Peuge Benjamin <Support@atomicdsolutions.com>
2016-04-22 00:18:42 -0400

    Change to Play

--- app/src/main/java/com/example/peugebenjamin/kings/Play.java
+++ app/src/main/java/com/example/peugebenjamin/kings/Play.java
@@ -1,34 +1,43 @@
 package com.example.peugebenjamin.kings;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Play extends AppCompatActivity {
+
+
+public class Play extends AppCompatActivity implements View.OnClickListener {
+    Button draw;
     Deck gamecards;
     Cards pulled;
-    TextView played;
+    String msg;
+    TextView playedcards;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        Button play = (Button) findViewById(R.id.btplay);
-        played = (TextView) findViewById(R.id.played);
-
-       // gamecards = new Deck();
-
+        playedcards = (TextView) findViewById(R.id.played);
+        draw = (Button) findViewById(R.id.btplay);
+        draw.setOnClickListener(this);
+        gamecards = new Deck();
     }
 
-    public void playGame(){
-
-            //pulled = gamecards.drawCard();
-            Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG).show();
-            //played.setText(pulled.toString());
+    @Override
+    public void onClick(View v) {
+        pulled = gamecards.drawCard();
+        if (pulled != null)
+            msg = gamecards.drawCard().toString();
+        else {
+            msg = "Not a Value ";
+        }
+        Toast.makeText(getApplicationContext(), msg , Toast.LENGTH_LONG).show();
+        playedcards.setText(msg.toCharArray(),0,msg.toCharArray().length);
     }
 }
